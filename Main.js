8708113544cbf9d741b308be0edf43b713b951cc
prodbywerty <klopojed.son@gmail.com>
2024-02-22 11:53:08 +0200

    talkbox world poop

--- Main.js
+++ Main.js
@@ -1,7 +1,5 @@
-
-//  Function for appending scripts
-function include( file ) {
-
+// Function for appending scripts
+function include(file) {
 	let script = document.createElement('script');
 	script.src = file;
 	script.type = 'text/javascript';
@@ -10,14 +8,14 @@ function include( file ) {
 	document.getElementsByTagName('head').item(0).appendChild(script);
 }
 
-function vec3( x = 0, y = 0, z = 0 ) {
-	return { x : x, y : y, z : z }
+function vec3(x = 0, y = 0, z = 0) {
+	return {x: x, y: y, z: z}
 }
-function getTransform( position = { x : 0, y : 0, z : 0 }, rotation = { x : 0, y : 0, z : 0 } ) { //translateZ( 600px )
-	return `rotateX( ${ rotation.x }deg ) rotateY( ${ rotation.y }deg ) translate3d(${ position.x }px, ${ position.y }px, ${ position.z }px)`
+function getTransform(position = {x: 0, y: 0, z: 0}, rotation = {x: 0, y: 0, z: 0}) {
+	return `rotateX( ${rotation.x}deg ) rotateY( ${rotation.y}deg ) translate3d(${position.x}px, ${position.y}px, ${position.z}px)`
 }
 
-//  File appending
-include( "js/map.js" )
-include( "js/world.js" )
-include( "js/player.js" )
+// File appending
+include("js/map.js")
+include("js/world.js")
+include("js/player.js")
